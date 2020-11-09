1b37575675f221340c002433bab376bc94cfabbc
sakamoto-takaya <takatakya-no11@ezweb.ne.jp>
2020-11-09 14:58:14 +0900

    ファイルの整理

--- crane_x7_examples/scripts/try.py
+++ crane_x7_examples/scripts/try.py
@@ -20,6 +20,7 @@ def main():
 
     print("---起動")
     rospy.sleep(1.0)
+
     # 何かを掴んでいた時のためにハンドを開く
     gripper.set_joint_value_target([0.9, 0.9])
     gripper.go()
@@ -28,36 +29,36 @@ def main():
     arm.set_named_target("home")
     arm.go()
 
+    # ハンドを少し閉じる
+    gripper.set_joint_value_target([0.7, 0.7])
+    gripper.go()
+
+    waypoints = []
+
+    target_pose = arm.get_current_pose().pose
+
+    # 上方から掴みに行く場合
+    q = quaternion_from_euler(-3.14/2.0, 0.0, -3.14/2.0)
+    target_pose.orientation.x = q[0]
+    target_pose.orientation.y = q[1]
+    target_pose.orientation.z = q[2]
+    target_pose.orientation.w = q[3]
+    waypoints.append(copy.deepcopy(wpose)) # Waypointの追加
+
     print("ウンコの消毒を開始")
     rospy.sleep(1.0)
     print("Step.1　ウンコを掴む")
     rospy.sleep(1.0)
 
-    target_pose = geometry_msgs.msg.Pose()
-    target_pose.position.x = 0.0
+    target_pose.position.x = 0.1
     target_pose.position.y = -0.3
-    target_pose.position.z = 0.05
-    q = quaternion_from_euler(-3.14/2.0, 0.0, -3.14/2.0)  # 上方から掴みに行く場合
-    target_pose.orientation.x = q[0]
-    target_pose.orientation.y = q[1]
-    target_pose.orientation.z = q[2]
-    target_pose.orientation.w = q[3]
-    arm.set_pose_target(target_pose)  # 目標ポーズ設定
-    arm.go()  # 実行
+    target_pose.position.z = 0.07
+    waypoints.append(copy.deepcopy(wpose))
 
     rospy.sleep(1.5)
 
-    target_pose = geometry_msgs.msg.Pose()
-    target_pose.position.x = 0.25
-    target_pose.position.y = -0.3
-    target_pose.position.z = 0.05
-    q = quaternion_from_euler(-3.14/2.0, 0.0, -3.14/2.0)  # 上方から掴みに行く場合
-    target_pose.orientation.x = q[0]
-    target_pose.orientation.y = q[1]
-    target_pose.orientation.z = q[2]
-    target_pose.orientation.w = q[3]
-    arm.set_pose_target(target_pose)  # 目標ポーズ設定
-    arm.go()  # 実行
+    wpose.position.x += 0.15
+    waypoints.append(copy.deepcopy(wpose))
 
     gripper.set_joint_value_target([0.3, 0.3])
     gripper.go()
@@ -65,58 +66,24 @@ def main():
     print("Step.2　ウンコを移動させる")
     rospy.sleep(1.0)
 
-    target_pose = geometry_msgs.msg.Pose()
-    target_pose.position.x = 0.25
-    target_pose.position.y = -0.3
-    target_pose.position.z = 0.1
-    q = quaternion_from_euler(-3.14/2.0, 0.0, -3.14/2.0)  # 上方から掴みに行く場合
-    target_pose.orientation.x = q[0]
-    target_pose.orientation.y = q[1]
-    target_pose.orientation.z = q[2]
-    target_pose.orientation.w = q[3]
-    arm.set_pose_target(target_pose)  # 目標ポーズ設定
-    arm.go()  # 実行
+    wpose.position.z += 0.1
+    waypoints.append(copy.deepcopy(wpose))
 
-    target_pose = geometry_msgs.msg.Pose()
-    target_pose.position.x = 0.25
-    target_pose.position.y = 0.15
-    target_pose.position.z = 0.1
-    q = quaternion_from_euler(-3.14/2.0, 0.0, -3.14/2.0)  # 上方から掴みに行く場合
-    target_pose.orientation.x = q[0]
-    target_pose.orientation.y = q[1]
-    target_pose.orientation.z = q[2]
-    target_pose.orientation.w = q[3]
-    arm.set_pose_target(target_pose)  # 目標ポーズ設定
-    arm.go()							# 実行
+    wpose.position.y += 0.45
+    waypoints.append(copy.deepcopy(wpose))
 
-    target_pose = geometry_msgs.msg.Pose()
-    target_pose.position.x = 0.25
-    target_pose.position.y = 0.15
-    target_pose.position.z = 0.05
-    q = quaternion_from_euler(-3.14/2.0, 0.0, -3.14/2.0)  # 上方から掴みに行く場合
-    target_pose.orientation.x = q[0]
-    target_pose.orientation.y = q[1]
-    target_pose.orientation.z = q[2]
-    target_pose.orientation.w = q[3]
-    arm.set_pose_target(target_pose)  # 目標ポーズ設定
-    arm.go()							# 実行
+    rospy.sleep(0.5)
+
+    wpose.position.z -= 0.1
+    waypoints.append(copy.deepcopy(wpose))
 
     gripper.set_joint_value_target([0.7, 0.7])
     gripper.go()
 
     rospy.sleep(1.0)
 
-    target_pose = geometry_msgs.msg.Pose()
-    target_pose.position.x = 0.2
-    target_pose.position.y = 0.15
-    target_pose.position.z = 0.05
-    q = quaternion_from_euler(-3.14/2.0, 0.0, -3.14/2.0)  # 上方から掴みに行く場合
-    target_pose.orientation.x = q[0]
-    target_pose.orientation.y = q[1]
-    target_pose.orientation.z = q[2]
-    target_pose.orientation.w = q[3]
-    arm.set_pose_target(target_pose)  # 目標ポーズ設定
-    arm.go()							# 実行
+    wpose.position.x -= 0.05
+    waypoints.append(copy.deepcopy(wpose))
 
     arm.set_named_target("home")
     arm.go()
@@ -135,6 +102,10 @@ def main():
     target_pose.orientation.w = q[3]
     arm.set_pose_target(target_pose)  # 目標ポーズ設定
     arm.go()  # 実行
+
+    arm_initial_pose = arm.get_current_pose().pose
+    print("Arm initial pose:")
+    print(arm_initial_pose)
 
     target_pose = geometry_msgs.msg.Pose()
     target_pose.position.x = 0.25
