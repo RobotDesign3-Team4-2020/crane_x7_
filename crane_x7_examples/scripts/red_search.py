4b6c2020b40d979d5dee301ab5299646a579018a
k-Ryunosuke <ryunosuke@k.com>
2020-12-12 14:32:03 +0900

    赤い円と緑の角度を検知して手首を回す

--- crane_x7_examples/scripts/red_search.py
+++ crane_x7_examples/scripts/red_search.py
@@ -17,11 +17,16 @@ from control_msgs.msg import (
     GripperCommandAction,
     GripperCommandGoal
  )
+from std_msgs.msg import Float64
 from trajectory_msgs.msg import JointTrajectoryPoint
 import math
 import sys
 import numpy as np
 from geometry_msgs.msg import Pose2D
+
+global ikkai
+ikkai = 0
+
 class ArmJointTrajectoryExample(object):
     def __init__(self):
         self._client = actionlib.SimpleActionClient(
@@ -58,28 +63,77 @@ class ArmJointTrajectoryExample(object):
     def callback(self, ms):
         global mode
         global count
-        
-        if ms.theta == 1: #ms.theta == 1 no notoki kiiro naiyo 
-                
+        global rad
+        if ms.theta >= 1: #ms.theta == 1 no notoki kiiro naiyo 
             if count < 200:
                 global sum_x
                 sum_x += ms.x #軸変換(センサのy軸をマニピュレータのx軸に合わせる)
                 global sum_y
                 sum_y += ms.y #軸変換(センサのx軸をマニピュレータのy軸に合わせる)
                 count += 1
+                print(ms.theta)
             else:
+                rad = ms.theta
                 print("ave_x :{}".format(sum_x/count))
                 print("ave_y :{}".format(sum_y/count))
-                self.calculate(sum_x/count,sum_y/count) #平均取ったものをself.calculateを送る
+                print("theta")
+                print(rad)
+                if rad < 160:
+                    if rad > 1:
+                        self.calculate(rad,sum_x/count,sum_y/count) #平均取ったものをself.calculateを送る
         else :
             print("-----------------\nCan't find unko\n----------------------")
             self.num +=1
-            print(self.num)
-            self.search(self.num)
+            print(ms.theta)
 
         print("count = {}".format(count))
 
-    def calculate(self, ave_x, ave_y):
+
+    def callback2(self, angle,tar_x,tar_y):
+        print ("nyannyan")
+        global mode
+        global count
+        radian_angle = angle * 3.14/180    
+        if radian_angle != 1.0:
+            if radian_angle < 2.4:
+                if radian_angle > 0.8:
+                    self.angle_change(radian_angle,tar_x,tar_y)
+
+
+    def angle_change(self, angle,tar_x,tar_y):
+        robot = moveit_commander.RobotCommander()
+        arm = moveit_commander.MoveGroupCommander("arm")
+        arm.set_max_velocity_scaling_factor(0.1)
+        gripper = moveit_commander.MoveGroupCommander("gripper")
+
+
+        while len([s for s in rosnode.get_node_names() if 'rviz' in s]) == 0:
+            rospy.sleep(1.0)
+        
+        target_pose = geometry_msgs.msg.Pose()
+        
+        
+        #つかむ前の位置、姿勢 
+        target_pose.position.x = tar_x
+        target_pose.position.y = tar_y
+        target_pose.position.z = 0.33
+        q = quaternion_from_euler(-3.14, 0.0, angle)
+        target_pose.orientation.x = q[0]
+        target_pose.orientation.y = q[1]
+        target_pose.orientation.z = q[2]
+        target_pose.orientation.w = q[3]
+        arm.set_pose_target(target_pose)
+        arm.go()
+        rospy.sleep(5.0)
+       
+
+        
+
+        #self.go(mode)
+
+
+
+    def calculate(self,angle, ave_x, ave_y):
         
         print("ave_x:{}".format(ave_x))
         print("ave_y:{}".format(ave_y))
@@ -91,7 +145,7 @@ class ArmJointTrajectoryExample(object):
         print("calculated\n res_x:{}".format(res_x))
         print("res_y:{}".format(res_y))
 
-        self.pick(res_x, res_y)
+        self.pick(angle,res_x, res_y)
 
     def search(self, num):
         robot = moveit_commander.RobotCommander()
@@ -157,7 +211,7 @@ class ArmJointTrajectoryExample(object):
     
            
     
-    def pick(self, res_x, res_y):
+    def pick(self, angle,res_x, res_y):
        
         robot = moveit_commander.RobotCommander()
         arm = moveit_commander.MoveGroupCommander("arm")
@@ -178,7 +232,7 @@ class ArmJointTrajectoryExample(object):
         #つかむ前の位置、姿勢 
         target_pose.position.x = tar_x
         target_pose.position.y = tar_y
-        target_pose.position.z = 0.15
+        target_pose.position.z = 0.33
         q = quaternion_from_euler(math.pi, 0.0, math.pi/2.0)
         target_pose.orientation.x = q[0]
         target_pose.orientation.y = q[1]
@@ -188,19 +242,15 @@ class ArmJointTrajectoryExample(object):
         arm.go() 
        
         #つかむ位置、姿勢
-        target_pose.position.z = 0.11
+        target_pose.position.z = 0.33
         arm.set_pose_target(target_pose)
         arm.go() 
 
         #ハンドを閉じる
         gripper.set_joint_value_target([0.2,0.2])
         gripper.go()
-       
-        #homeへ 
-        arm.set_named_target("home")
-        arm.go()
-        mode = 1
-        #self.go(mode)
+        global rad_s
+        self.callback2(angle,tar_x,tar_y)
 
 
 if __name__ == "__main__":
