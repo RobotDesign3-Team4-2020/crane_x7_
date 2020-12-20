810e3d9bce18496a7f0da0a63e7a40bace98ebb8
k-Ryunosuke <ryunosuke@k.com>
2020-12-21 05:16:07 +0900

    パッケージ名変更

---
+++ s-disinfect/scripts/green_angle.py
@@ -0,0 +1,181 @@
+#!/usr/bin/env python
+# coding: utf-8
+#searching pose
+
+import rospy
+import time
+import actionlib
+from control_msgs.msg import (
+    FollowJointTrajectoryAction,
+    FollowJointTrajectoryGoal
+)
+import geometry_msgs.msg 
+import moveit_commander
+import rosnode
+from tf.transformations import quaternion_from_euler, euler_from_quaternion 
+from control_msgs.msg import (      
+    GripperCommandAction,
+    GripperCommandGoal
+ )
+from trajectory_msgs.msg import JointTrajectoryPoint
+import math
+import sys
+import numpy as np
+from geometry_msgs.msg import Pose2D
+
+
+class ArmJointTrajectoryExample(object):
+    def __init__(self):
+        self._client = actionlib.SimpleActionClient(
+            "/crane_x7/arm_controller/follow_joint_trajectory", FollowJointTrajectoryAction)
+        rospy.sleep(0.1)
+        if not self._client.wait_for_server(rospy.Duration(secs=5)):
+            rospy.logerr("Action Server Not Found")
+            rospy.signal_shutdown("Action Server Not Found")
+            sys.exit(1)
+        self.gripper_client = actionlib.SimpleActionClient("/crane_x7/gripper_controller/gripper_cmd",GripperCommandAction)
+        self.gripper_goal = GripperCommandGoal()
+        # Wait 5 Seconds for the gripper action server to start or exit
+        self.gripper_client.wait_for_server(rospy.Duration(5.0))
+        self.num = 0
+        if not self.gripper_client.wait_for_server(rospy.Duration(5.0)):
+            rospy.logerr("Exiting - Gripper Action Server Not Found.")
+            rospy.signal_shutdown("Action Server not found.")
+            sys.exit(1)
+        #self.sub = rospy.Subscriber("bool", Pose2D, self.callback)
+         
+
+          
+ 
+    def callback(self, ms):
+        global radian_angle 
+        global mode
+        global count
+        radian_angle = ms.theta * 3.14/180    
+        print ms.theta
+        if radian_angle < 2.4:
+            if radian_angle > 0.8:
+                self.pick(radian_angle)
+    
+
+    
+    def search(self, num):
+        robot = moveit_commander.RobotCommander()
+        arm = moveit_commander.MoveGroupCommander("arm")
+        arm.set_max_velocity_scaling_factor(0.1)
+        gripper = moveit_commander.MoveGroupCommander("gripper")
+
+        while len([s for s in rosnode.get_node_names() if 'rviz' in s]) == 0:
+            rospy.sleep(1.0)
+        rospy.sleep(1.0)
+
+        print("Group names:")
+        print(robot.get_group_names())
+
+        print("Current state:")
+        #print(robot.get_current_state())
+
+        # アーム初期ポーズを表示
+        arm_initial_pose = arm.get_current_pose().pose
+        print("Arm initial pose:")
+        print(arm_initial_pose)
+
+        # 何かを掴んでいた時のためにハンドを開く
+        gripper.set_joint_value_target([0.9, 0.9])
+        gripper.go()
+
+        # SRDFに定義されている"home"の姿勢にする
+        arm.set_named_target("home")
+        arm.go()
+        gripper.set_joint_value_target([0.7, 0.7])
+        gripper.go()
+          
+        target_pose = geometry_msgs.msg.Pose()
+        
+        #つかむ前の姿勢
+        search_x = 0.2
+        search_y = 0.1
+        search_z = 0.33
+        global x
+        x = search_x 
+        global y
+        y = search_y
+        global z
+        z = search_z 
+        target_pose.position.x = search_x
+        target_pose.position.y = search_y
+        target_pose.position.z = search_z
+        q = quaternion_from_euler(-3.14, 0, 3.14/2.0)  # 上方から掴みに行く場合
+        target_pose.orientation.x = q[0]
+        target_pose.orientation.y = q[1]
+        target_pose.orientation.z = q[2]
+        target_pose.orientation.w = q[3]
+        
+        arm.set_pose_target(target_pose)  # 目標ポーズ設定
+        arm.go()
+        
+        print("go end") 
+        rospy.sleep(2.0)
+        print("sleep end")
+        global mode
+        mode = 0 
+        self.sub = rospy.Subscriber("bool", Pose2D, self.callback)
+    
+           
+    
+           
+    
+    def pick(self, angle):
+        robot = moveit_commander.RobotCommander()
+        arm = moveit_commander.MoveGroupCommander("arm")
+        arm.set_max_velocity_scaling_factor(0.1)
+        gripper = moveit_commander.MoveGroupCommander("gripper")
+
+
+        while len([s for s in rosnode.get_node_names() if 'rviz' in s]) == 0:
+            rospy.sleep(1.0)
+        
+        target_pose = geometry_msgs.msg.Pose()
+        
+        offset_x = 0.05            #カメラと手先のオフセット
+        offset_y = 0.03            #カメラと手先のオフセット
+        
+        #tar_x = res_y + x + offset_x                        #センサの位置にあった原点をマニピュレータの根元へ移動
+        #tar_y = -res_x + y + offset_y             #センサの位置にあった原点をマニピュレータの根元へ移動
+        
+        #つかむ前の位置、姿勢 
+        target_pose.position.x = 0.2
+        target_pose.position.y = 0.1
+        target_pose.position.z = 0.33
+        q = quaternion_from_euler(-3.14, 0.0, angle)
+        target_pose.orientation.x = q[0]
+        target_pose.orientation.y = q[1]
+        target_pose.orientation.z = q[2]
+        target_pose.orientation.w = q[3]
+        arm.set_pose_target(target_pose)
+        arm.go()
+        rospy.sleep(5.0)
+       
+        #つかむ前の位置、姿勢 
+        target_pose.position.x = 0.2
+        target_pose.position.y = 0.1
+        target_pose.position.z = 0.33
+        q = quaternion_from_euler(-3.14, 0.0, 0.0)
+        target_pose.orientation.x = q[0]
+        target_pose.orientation.y = q[1]
+        target_pose.orientation.z = q[2]
+        target_pose.orientation.w = q[3]
+        arm.set_pose_target(target_pose)
+        arm.go()  
+        print("nyannnyann")
+        
+
+        #self.go(mode)
+
+
+if __name__ == "__main__":
+    rospy.init_node("arm_joint_trajectory_example")
+    arm_joint_trajectory_example = ArmJointTrajectoryExample() 
+    arm_joint_trajectory_example.search(0)
+    rospy.spin()
+
---
+++ s-disinfect/scripts/test.cpp
@@ -0,0 +1,273 @@
+#include <ros/ros.h>
+#include <sensor_msgs/Image.h>
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/opencv.hpp>
+#include <geometry_msgs/Pose2D.h>
+#include <vector>
+#include<std_msgs/Float64MultiArray.h>
+#include <algorithm>
+
+int time_count=0;
+using namespace::cv;
+geometry_msgs::Pose2D ms;
+//std::string msg; 
+class depth_estimater{
+public:          //変数をpublicで宣言
+    depth_estimater();
+    ~depth_estimater();
+    void rgbImageCallback(const sensor_msgs::ImageConstPtr& msg);
+    void depthImageCallback(const sensor_msgs::ImageConstPtr& msg);
+ 
+
+private:     //hensuu wo private de sengen
+    ros::NodeHandle nh;
+    ros::Subscriber sub_rgb, sub_depth;
+    ros::Publisher pub0 = nh.advertise<geometry_msgs::Pose2D>("bool",1);
+    ros::Publisher pub1 = nh.advertise<geometry_msgs::Pose2D>("bool1",1);
+    ros::Publisher pub2 = nh.advertise<geometry_msgs::Pose2D>("bool2",1);
+    double bunbo = 0;
+    double sumx, sumy;
+};
+cv::Mat img_1;  //int x; mitai na mono //gazou wo kakunou suru hennsuu no sengen 
+cv::Mat input = cv::imread("/camera/color/image_raw");
+depth_estimater::depth_estimater(){
+    //RealSense
+    //sub_rgb = nh.subscribe<sensor_msgs::Image>("/usb_cam/image_raw", 1, &depth_estimater::rgbImageCallback, this);
+	//シミュレータ上のカメラ
+    sub_rgb = nh.subscribe<sensor_msgs::Image>("/camera/color/image_raw", 1, &depth_estimater::rgbImageCallback, this);
+}
+ 
+depth_estimater::~depth_estimater(){
+}
+ 
+
+void depth_estimater::rgbImageCallback(const sensor_msgs::ImageConstPtr& msg){
+    cv_bridge::CvImagePtr cv_ptr;
+ /*if(time_count<800){
+    time_count++;
+    try{
+        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }catch (cv_bridge::Exception& ex){
+        ROS_ERROR("error");
+        exit(-1);
+    }    
+    cv::Mat hsv_img;
+    cvtColor( cv_ptr->image,hsv_img,CV_BGR2HSV,3);
+   
+    
+    //Scalar lower = cv::Scalar(0,200,200);
+    //Scalar upper = cv::Scalar(30,255,255);    
+    Scalar lower = cv::Scalar( 155, 50,  50); //フィルタリングする色の範囲
+    Scalar upper = cv::Scalar( 180, 255, 255);
+
+
+	// BGRからHSVへ変換
+	Mat mask_image, output_image;
+    int px_1,x,y,x_mem,y_mem;
+    int flag = 0;
+    // inRangeを用いてフィルタリング
+	inRange(hsv_img, lower, upper, mask_image);
+    int count = 0;
+	
+    
+    // 輪郭を格納するcontoursにfindContours関数に渡すと輪郭を点の集合として入れてくれる
+    std::vector<std::vector<cv::Point> > contours;
+    cv::findContours(mask_image, contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);    // 輪郭線を格納
+
+
+    double max_area=0;
+    
+    int max_area_contour=-1;
+    try{
+        // 各輪郭をcontourArea関数に渡し、最大面積を持つ輪郭を探す
+        for(int j=0; j<contours.size(); j++){
+            double area = cv::contourArea(contours.at(j)); //menseki keisan
+            if(max_area<area){
+                max_area=area;
+                max_area_contour=j;
+            }
+        }
+    }catch(CvErrorCallback){
+        
+    }
+    if(max_area_contour != -1){
+        int counts = contours.at(max_area_contour).size();
+        double gx = 0, gy = 0;    //gx,gyはオブジェクトの重心の座標
+    
+        for(int k = 0; k < counts; k++){
+            gx += contours.at(max_area_contour).at(k).x;
+            gy += contours.at(max_area_contour).at(k).y;
+        }
+         
+        gx/=counts;  
+        gy/=counts;
+        ms.x = -320 + gx;    //ms.xは中心を(0,0)としたときのxの値 (画像の右方向をx軸の正とする)
+        ms.y = 240 - gy;    //ms.yは中心を(0,0)としたときのyの値 (画像の上方向をy軸の正とする)
+        ms.theta = 1;
+        printf("x = %lf, y = %lf theta = %lf\n", ms.x, ms.y, ms.theta);
+        pub0.publish(ms);
+        pub1.publish(ms);
+        pub2.publish(ms);
+    }else{
+        ms.theta = 0;
+        pub0.publish(ms);
+        pub1.publish(ms);
+        pub2.publish(ms);
+        printf("x = %lf, y = %lf theta = %lf\n", ms.x, ms.y, ms.theta);
+    } 
+printf("timecount=%d\n", time_count);
+
+    // マスクを基に入力画像をフィルタリング
+    cv_ptr->image.copyTo(output_image, mask_image);
+	for( y = 0;y < 480;y++){
+        for( x = 0; x < 640; x++){
+            px_1 = static_cast<int>(output_image.at<unsigned char>(y, x));  
+            if(px_1 > 200 && flag == 0){
+               
+                x_mem = x;
+                y_mem = y;
+                flag = 1;
+            }
+        }
+    }
+    if(flag == 1){
+        cv::rectangle(output_image,cv::Point(x_mem-200,y_mem),cv::Point(x_mem-500,y_mem+300),cv::Scalar(0,200,0),3,4);
+        //std::cout << x_mem << "," << y_mem << std::endl;
+        flag = 0;
+    }
+    cv::imshow("RGB image", output_image);
+    cv::waitKey(10);
+}
+
+
+
+
+else{*/
+    try{
+        cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+    }catch (cv_bridge::Exception& ex){
+        ROS_ERROR("error");
+        exit(-1);
+    }    
+    cv::Mat hsv_img;
+    cvtColor( cv_ptr->image,hsv_img,CV_BGR2HSV,3);
+   
+    
+    //Scalar lower = cv::Scalar(0,200,200);
+    //Scalar upper = cv::Scalar(30,255,255);    
+    Scalar lower = cv::Scalar( 0, 120,  120); //フィルタリングする色の範囲
+    Scalar upper = cv::Scalar( 80, 230, 255);
+
+
+	// BGRからHSVへ変換
+	Mat mask_image, output_image;
+    int px_1,x,y,x_mem,y_mem;
+    int flag = 0;
+    double gx = 0, gy = 0;    //gx,gyはオブジェクトの重心の座標
+
+    // inRangeを用いてフィルタリング
+	inRange(hsv_img, lower, upper, mask_image);
+    int count = 0;
+	
+    
+    // 輪郭を格納するcontoursにfindContours関数に渡すと輪郭を点の集合として入れてくれる
+    std::vector<std::vector<cv::Point> > contours2;
+    cv::findContours(mask_image, contours2, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);    // 輪郭線を格納
+    cv::Point2f rect_points[4];//長方形4点の座標
+    cv::Point2f edge1;
+    cv::Point2f edge2;
+    cv::Point2f usedEdge;
+    cv::Point2f center;
+
+    double max_area=0;
+    double angle = 0;
+    
+    int max_area_contour=-1;
+    try{
+        // 各輪郭をcontourArea関数に渡し、最大面積を持つ輪郭を探す
+        for(int j=0; j<contours2.size(); j++){
+            cv::RotatedRect rotatedRect = cv::minAreaRect(contours2.at(j)); //menseki keisan
+            double area = cv::contourArea(contours2.at(j)); //menseki keisan
+            rotatedRect.points(rect_points);
+            angle = rotatedRect.angle;
+            center = rotatedRect.center;
+            if(max_area<area){
+                max_area=area;
+                max_area_contour=j;
+            }
+            
+        }
+    }catch(CvErrorCallback){
+        }
+        if(max_area_contour != -1){
+            edge1 = cv::Vec2f(rect_points[1].x,rect_points[1].y)-cv::Vec2f(rect_points[0].x,rect_points[0].y);
+            edge2 = cv::Vec2f(rect_points[2].x,rect_points[2].y)-cv::Vec2f(rect_points[1].x,rect_points[1].y);
+            usedEdge = edge1;
+            if(cv::norm(edge2) > cv::norm(edge1)){
+                usedEdge = edge2;
+            }
+            cv::Point2f reference = cv::Vec2f(1,0);
+            angle = 180.0/CV_PI * acos((reference.x * usedEdge.x + reference.y * usedEdge.y) / (cv::norm(reference) * cv::norm(usedEdge)));
+            for(int j=0;j<4;j++){
+                cv::line(input, rect_points[j],rect_points[(j+1)%3],cv::Scalar(0,255,0));
+            }
+            std::stringstream ss; ss<<angle;
+            //cv::circle(input,center,5,cv::Scalar(0,255,0));
+            //cv::putText(input,ss.str(),center+ cv::Point2f(-25,25),cv::FONT_HERSHEY_COMPLEX_SMALL,1,cv::Scalar(255,0,255));
+            std::cout << "angle" << ms.theta << std::endl;
+            ms.theta = angle;
+
+
+        int counts = contours2.at(max_area_contour).size();
+        double gx = 0, gy = 0;    //gx,gyはオブジェクトの重心の座標
+    
+        for(int k = 0; k < counts; k++){
+            gx += contours2.at(max_area_contour).at(k).x;
+            gy += contours2.at(max_area_contour).at(k).y;
+        }
+         
+        gx/=counts;  
+        gy/=counts;
+        ms.x = -320 + gx;    //ms.xは中心を(0,0)としたときのxの値 (画像の右方向をx軸の正とする)
+        ms.y = 240 - gy;    //ms.yは中心を(0,0)としたときのyの値 (画像の上方向をy軸の正とする)            
+
+
+            std::cout <<"x"<< ms.x << "," << "y" <<ms.y << std::endl;
+            pub0.publish(ms);
+            pub1.publish(ms);
+            pub2.publish(ms);
+        }
+
+    // マスクを基に入力画像をフィルタリング
+    cv_ptr->image.copyTo(output_image, mask_image);
+	for( y = 0;y < 480;y++){
+        for( x = 0; x < 640; x++){
+            px_1 = static_cast<int>(output_image.at<unsigned char>(y, x));  
+            if(px_1 > 200 && flag == 0){
+               
+                x_mem = x;
+                y_mem = y;
+                flag = 1;
+            }
+        }
+    }
+    if(flag == 1){
+        cv::rectangle(output_image,cv::Point(x_mem-200,y_mem),cv::Point(x_mem-500,y_mem+300),cv::Scalar(0,200,0),3,4);
+        //std::cout << x_mem << "," << y_mem << std::endl;
+        flag = 0;
+    }
+    cv::imshow("RGB image", output_image);
+    cv::waitKey(10);
+//}
+
+
+}
+int main(int argc, char **argv){
+    sleep(2.0);
+    ros::init(argc, argv, "depth_estimater");
+    depth_estimater depth_estimater;
+    ros::spin();
+    return 0;
+}
+
+
